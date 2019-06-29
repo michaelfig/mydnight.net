75d38f3023c11eae0c6cf86f84da124fd199e1f4
Michael FIG <michael+github@fig.org>
2019-06-29 12:27:16 -0600

    Update cemetary link.

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ and publishing it for the future. The live stream, recording and other contribut
 Stewart’s memory will be available from <a href="https://mydnight.net/">mydnight.net</a>.</p>
 
 <p>The memorial will be held Sunday July 7th, at <a target="_blank" href="http://www.timebie.com/std/cst.php?q=13">1pm Central Standard Time</a>,
-in the <a target="_blank" href="https://goo.gl/maps/9SdmvWqRDLo85dyz5">Oxbow (Saskatchewan) Seniors' Centre</a>. A graveside committal at the
-Glen Ewen cemetery will occur following the memorial, with a brief lunch to
+in the <a target="_blank" href="https://goo.gl/maps/9SdmvWqRDLo85dyz5">Oxbow (Saskatchewan) Seniors' Centre</a>. A graveside committal at 
+the <a href="https://goo.gl/maps/ESmHqLmF5zjdME5S9">Glen Ewen</a> cemetery will occur following the memorial, with a brief lunch to
 follow. If you are in need of assistance seating at the gravesite, please feel
 free to bring your own chair.  Please
-visit <a href="https://mydnight.net">https://mydnight.net</a> for updated
+visit <a href="https://mydnight.net/">https://mydnight.net</a> for updated
 information and <a href="#rsvp">to fill out the RSVP form</a>.</p>
 
 <p>As per <a target="_blank" href="https://www.redpathfuneralhome.com/notices/Stewart-Berntson?fbclid=IwAR3WLUKxbXjvX7QA6i4QoTDGbrexUSV0_ZWnJDvi_d4nu40t9VwAF6Lqi-U">the obituary</a>,
