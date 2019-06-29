1fde9ee59bd704c93e2e2d5d4a5cd027cb3cdd19
Michael FIG <michael+github@fig.org>
2019-06-29 09:17:30 -0600

    Almost up-to-date.

---
+++ TODO.md
@@ -0,0 +1,8 @@
+* Participant venues/capacity
+  * Venue coordinators
+  * Every venue has audio-only (cell phone) + audio/video endpoints
+  * Cancellable RSVP with number attendees, venue
+
+* 3-up-next app, with venue, name (optional last name), relationship to Stewart
+
+* Video or audio-only
--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>Our dear Stewart passed away unexpectedly Thursday, June 27, 2019 at the age of 33.</p>
+
+<p>Reflecting the thoughtful and heartfelt spirit in which Stewart shared his many stories,
+  we will be holding a memorial service where participants can reflect, and choose to share
+  something from the heart.  You are free to prepare something to share (thoughts, feelings,
+  a poem, some music, or other), or do something impromptu as you feel led.</p>
+
+<p>You can consider whether you want to go up to the podium and be recorded or share audio only
+at a microphone from your seat.  You will also have the option of having your name and
+relationship to Stewart displayed on screen to the participants.</p>
+
+<p>In honor of Stewart’s career, we will be live streaming the memorial service, and recording
+and publishing it for the future. The live stream, recording and other contributions in
+Stewart’s memory will be available from <a href="https://mydnight.net/">mydnight.net</a>.</p>
+
+<p>The memorial will be held Sunday July 7th, at <a href="http://www.timebie.com/std/cst.php?q=13">1pm Central Standard Time</a>,
+in the <a href="https://goo.gl/maps/qSHv6ruz43baCQYr7">Oxbow, Saskatchewan area</a>.  <b>Whether you can or cannot attend, please fill out the <a href="#rsvp">RSVP form below</a></b>.
+</p>
+
+<p>As per <a href="https://www.redpathfuneralhome.com/notices/Stewart-Berntson?fbclid=IwAR3WLUKxbXjvX7QA6i4QoTDGbrexUSV0_ZWnJDvi_d4nu40t9VwAF6Lqi-U">the obituary</a>,
+donations in memory of Stewart may be given to:
+  <blockquote><a href="https://www.epilepsysaskatoon.com/">Saskatchewan Epilepsy Society</a><br/>
+  114 Maxwell Crescent<br/>
+  Saskatoon, SK<br/>
+  CANADA&nbsp;&nbsp;&nbsp;S7L 3Y2</blockquote></p>
+
+<a name="rsvp"><iframe title="Stewart Berntson Memorial RSVP" src="https://docs.google.com/forms/d/e/1FAIpQLSf8ue-sMmm1rhS_Njr2OqwkPqnvYO9wN50gPeSc6ntX9EvDSQ/viewform?embedded=true" width="640" height="1516" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe></a>
+
       </header>
     </div>
   );
